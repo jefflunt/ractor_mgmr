4062628e31aa376bcd21d11a5a8304fcd7a74ed8
jefflunt <jefflunt@gmail.com>
2023-01-09 11:51:06 -0600

    adding tiny_eta

--- lib/ractor_mgmr.rb
+++ lib/ractor_mgmr.rb
@@ -1,3 +1,5 @@
+require 'tiny_eta'
+
 # RactorMgmr is a job queue combined with a monitor for a list of Ractors. the
 # job of a RactorMgmr is:
 #
@@ -61,16 +63,17 @@ class RactorMgmr
     @jobs_finished = 0
     @results = []
 
-    _start_work
-  end
+    ##
+    # start work
+    @start_time = Time.now
 
-  def _start_work
     # initial job fill
     @workers.each do |w|
       w.send(@jobs[@job_index])
       @job_index += 1
     end
 
+    # Thread for feeding workers
     @mgmr_thread = Thread.new do
       @status = :running
 
@@ -94,7 +97,7 @@ class RactorMgmr
   end
 
   def done?
-    @job_index == @jobs.length
+    jobs_finished == @jobs.length
   end
 
   # the total number of jobs that will be run
@@ -117,14 +120,31 @@ class RactorMgmr
     jobs_total - jobs_finished
   end
 
-  # the percentage complete (a floating point number between 0.0..100.0)
-  def percent_complete(digits=0)
+  # percentage complete as a Float in the range of 0.0..1.0
+  def percent_complete_f(digits=0)
+    ((jobs_finished / jobs_total.to_f) * 100)
+  end
+
+  # percentage complete (a floating point number between 0.0..100.0)
+  def percent_complete_s(digits=0)
     ((jobs_finished / jobs_total.to_f) * 100).round(digits)
+  end
+
+  # returns a TinyEta string for ETA time
+  #
+  # see: https://github.com/jefflunt/tiny_eta
+  def eta
+    TinyEta.eta(Time.now - @start_time, percent_complete_f)
   end
 
   # just like Thread#join, blocks until all jobs are complete
   def join
     @mgmr_thread.join
     nil
+  end
+
+  # out-of-the-box monitoring
+  def to_s
+    "RactorMgmr #{jobs_running} running, progress: #{jobs_finished}/#{jobs_total} #{percent_complete_s(1)}% (ETA #{eta})"
   end
 end
--- ractor_mgmr.gemspec
+++ ractor_mgmr.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "ractor_mgmr"
-  s.version     = "1.0.0"
+  s.version     = "1.1.0"
   s.description = "a job queue and monitor for Ractors"
   s.summary     = "want to have a job queue for a list of identical Ractors, and have a little manager that feeds them work as they become availble? then this library is for you"
   s.authors     = ["Jeff Lunt"]
@@ -8,4 +8,6 @@ Gem::Specification.new do |s|
   s.files       = ["lib/ractor_mgmr.rb"]
   s.homepage    = "https://github.com/jefflunt/ractor_mgmr"
   s.license     = "MIT"
+
+  s.add_runtime_dependency "tiny_eta", [">= 1.0.1"]
 end
